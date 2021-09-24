3cf13457cfc45bd22cdb35dd5087420da6e7735c
Orlando <tortonorlando.omg@gmail.com>
2021-09-24 18:05:25 -0500

    Missing changes

--- dist/user/user.controller.d.ts
+++ dist/user/user.controller.d.ts
@@ -1,12 +1,12 @@
-import { JwtService } from '@nestjs/jwt';
 import { Response } from 'express';
-import { User } from '../models/user';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '../../src/models/user';
 import { UserService } from './user.service';
 export declare class UserController {
-    private userService;
-    private jwtService;
-    constructor(userService: UserService, jwtService: JwtService);
-    getUser(res: Response): Promise<void>;
-    create(userData: User, res: Response): Promise<void>;
-    login(reqBody: User, res: Response): Promise<void>;
+  private userService;
+  private jwtService;
+  constructor(userService: UserService, jwtService: JwtService);
+  getUser(res: Response): Promise<void>;
+  create(userData: User, res: Response): Promise<void>;
+  login(reqBody: User, res: Response): Promise<void>;
 }
